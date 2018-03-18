001e115b003f273af367bb3477515b00ebfcd983
ziyed <ziyed.cse@gmail.com>
2018-03-18 06:59:44 +0600

    working

--- example.php
+++ example.php
@@ -3,21 +3,31 @@
 
 class Father{    
     public static $money = 100;
-	protected static $doller = 50;
-	Private $bankCheck = 85478;
+    protected static $doller = 50;
+    private static $bankCheck = 85478;
 	
-	public static function permission(){
-		echo 'Father Permission Granted: '.$this->bankCheck.'<br/>';
-	}
+    public static function permission(){
+        echo 'Father Permission Granted: '. self::$bankCheck.'<br/>';
+    }
 }
 class Son extends Father{
-	
-	public function property(){
-		$this->permission();
-	}
+    
+    public static function permission() {
+        echo 'inside child permission';
+    }
+
+    public function property(){        
+        parent::permission();
+    }
 		
 }
 
-echo Father::permission();
+//Father::permission();
+$obj = new Son();
+
+$obj->permission();
+
+///echo Father::$money;
+
 
 ?>
